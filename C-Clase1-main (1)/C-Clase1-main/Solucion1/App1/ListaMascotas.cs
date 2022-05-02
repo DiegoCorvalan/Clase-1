b3248483b6c809493e16800395bfd8a7c94cc84c
DiegoCorvalan <diegodarkcor@gmail.com>
2022-05-02 12:05:19 -0400

    j

---
+++ C-Clase1-main (1)/C-Clase1-main/Solucion1/App1/ListaMascotas.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    /// <summary>
+    /// Clase que permite almacenarr un arreglo de 10 mascotas
+    /// </summary>
+    internal class ListaMascotas
+    {
+        Mascota[] misMascotas=new Mascota[10];
+        private bool hayEspacio;
+
+        public ListaMascotas()
+        {
+            hayEspacio = true;
+        }
+        
+        /// <summary>
+        /// Clase que permite agregar una nueva mascota
+        /// en la primera posición null que encuentre.
+        /// Modifica el valor hayEspacio si ya no quedan 
+        /// posiciones disponibles
+        /// </summary>
+        /// <param name="nueva">Una nueva mascota</param>
+        /// <returns>True si pudo agregar la mascota, False
+        /// si no pudo</returns>
+        public bool AgregarMascota(Mascota nueva)
+        {
+            bool res = false;
+            int posicion = Disponible();
+            if (posicion>-1)
+            {
+                misMascotas[posicion] = nueva;
+                res = true;
+            }
+            return res;
+        }
+
+        private int Disponible()
+        {
+            int a=-1;
+            for (int i = 0; i < misMascotas.Length; i++)
+            {
+                if(misMascotas[i]==null)
+                {
+                    a = i;
+                    break;
+                }
+            }
+
+            return a;
+        }
+    }
+}
---
+++ C-Clase1-main (1)/C-Clase1-main/Solucion1/App1/Menu.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    internal class Menu
+    {
+        ListaMascotas lista = new ListaMascotas();
+
+        public void MostrarMenu()
+        {
+            bool salir = true;
+            string opcion = "";
+            while(salir)
+            {
+                Console.WriteLine("1. Agregar mascota");
+                Console.WriteLine("2. Buscar mascota");
+                Console.WriteLine("3. Borrar mascota");
+                Console.WriteLine("4. Modificar mascota");
+                Console.WriteLine("5. Salir");
+
+                opcion = Console.ReadLine();
+
+                switch(opcion)
+                {
+                    case "1":
+                        Console.WriteLine("Ingreso de nueva mascota");
+                        long codigo=0;
+                        string nombre;
+                        DateTime fechaNacimiento;
+                        string especie;
+                        string raza;
+                        bool funciona = true;
+
+                        while (funciona)
+                        {
+                            try
+                            {
+                                Console.WriteLine("Ingresa el código");
+                                codigo = long.Parse(Console.ReadLine());
+                                funciona = false;
+                            }
+                            catch(FormatException ex)
+                            {
+                                Console.WriteLine("El código sólo puede ser un número");
+                            }
+                        }
+                        Console.WriteLine("Ingresa el nombre");
+                        nombre = Console.ReadLine();
+
+
+                        Console.WriteLine("Ingresa la fecha");
+                        fechaNacimiento = DateTime.Parse(Console.ReadLine());
+
+
+                        Console.WriteLine("Ingresa la especie");
+                        especie = Console.ReadLine();
+
+                        Console.WriteLine("Ingresa la raza");
+                        raza=Console.ReadLine();
+
+                        Mascota m = new Mascota(codigo, nombre, fechaNacimiento, especie, raza);
+
+                        if (lista.AgregarMascota(m))
+                            Console.WriteLine("Mascota agregada con exito");
+                        else
+                            Console.WriteLine("Falló agregar la mascota");
+
+                        break;
+                    case "2":
+                        Console.WriteLine("Presionó 2");
+                        break;
+                    case "3":
+                        Console.WriteLine("Presionó 3");
+                        break;
+                    case "4":
+                        Console.WriteLine("Presionó 4");
+                        break;
+                    case "5":
+                        salir = false;
+                        Console.WriteLine("Gracias XD");
+                        Console.WriteLine("Presiona cualquier tecla para salir");
+                        Console.ReadKey();
+                        break;
+                    default:
+                        Console.WriteLine("Sólo valores entre 1 y 5 Gracias Imbécil");
+                        break;
+                }
+
+            }
+        }
+    }
+}
